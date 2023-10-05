630ee75e6385a5e67d3eb51a90367f2ec7dcf5f0
AyushJakhmola <ayushjakhmola001@gmail.com>
2023-10-05 18:08:12 +0530

    upgraded jenkins version

--- variables.tf
+++ variables.tf
@@ -17,6 +17,6 @@ variable "namespace" {
 
 variable "chart_version" {
   type        = string
-  default     = "4.5.0"
+  default     = "4.6.6"
   description = "Version of the Jenkins chart that will be used to deploy Jenkins application."
 }
